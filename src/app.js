64caae16b33bb0f16c36940e66fd803ed9466387
joshweaver23 <joshweaver23@gmail.com>
2017-06-12 22:37:14 -0400

    added image loading, bootstrap 3, and font awesome

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const css = require('./app.scss');
+import css from './app.scss';
+const FontAwesome = require("font-awesome-sass-loader");
 
 ReactDOM.render(
-	<h1>Hello, world!</h1>,
+	<div>
+		<h1>Hello, world!</h1>
+		<i className="fa fa-free-code-camp fa-lg" aria-hidden="true"></i>
+	</div>,
 	document.getElementById('root')
 );
